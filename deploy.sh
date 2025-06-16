7f4b6038b7f53dbe6ec8734381a6eaefab0f9f9d
wisneysouza <wisney.souza@gmail.com>
2025-06-16 02:53:13 -0300

    verificando textos das paginas

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+
+rm -rf ./backend/public/*
+
+
+cp -r ../2_clubes ./public
+cp -r ../3_quem_somos ./public
+cp -r ../4_forms ./public
+cp -r ../5_etica ./public
+cp -r ../css ./public
+cp -r ../img ./public
+cp -r ../js ./public
+cp -r ../footer.html ./public
+cp -r ../header.html ./public
+cp -r ../index.html ./public
