7c914bfc8f7a2ace5177e34076eae000f1390417
wisneysouza <wisney.souza@gmail.com>
2025-06-16 03:18:35 -0300

    alterando curriculo

--- js/inicio.js
+++ js/inicio.js
@@ -1,10 +1,10 @@
 // Usando fetch() para importar o header.html
-    fetch('header.html')
+    fetch('../header.html')
         .then(response => response.text())
         .then(data => {
         document.getElementById('header').innerHTML = data;
         });
-    fetch('footer.html')
+    fetch('../footer.html')
             .then(response => response.text())
             .then(data => {
             document.getElementById('footer').innerHTML = data;
